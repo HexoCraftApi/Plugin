a85a1dd3c5ba84ae6862d6d2055f453783a5f28c
hexosse <hexosse@gmail.com>
2016-09-21 14:05:57 +0200

    pom update and optimized imports

--- src/main/java/com/github/hexocraftapi/plugin/Plugin.java
+++ src/main/java/com/github/hexocraftapi/plugin/Plugin.java
@@ -18,6 +18,16 @@ package com.github.hexocraftapi.plugin;
 
 import com.github.hexocraftapi.command.Command;
 import com.github.hexocraftapi.command.CommandRegistration;
+import com.github.hexocraftapi.message.predifined.message.EmptyMessage;
+import com.github.hexocraftapi.message.predifined.message.ErrorMessage;
+import com.github.hexocraftapi.message.predifined.message.PluginMessage;
+import com.github.hexocraftapi.message.predifined.message.PluginStraightMessage;
+import com.github.hexocraftapi.updater.updater.Downloader;
+import com.github.hexocraftapi.updater.updater.Response;
+import com.github.hexocraftapi.updater.updater.Updater;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.IllegalPluginAccessException;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -43,5 +53,57 @@ public abstract class Plugin extends JavaPlugin
 			throw new IllegalPluginAccessException("Plugin attempted to register " + command + " while not enabled");
 
 		CommandRegistration.registerCommand(this, command);
+	}
+
+	public void runUpdater(final Updater updater, final CommandSender sender, int delay)
+	{
+		final JavaPlugin plugin = this;
+
+		Bukkit.getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Response response = updater.getResult();
+					if(response == Response.SUCCESS)
+					{
+						EmptyMessage.toSender(sender);
+						PluginStraightMessage.toSender(sender, plugin, ChatColor.AQUA);
+						PluginMessage.toSender(sender, plugin, "");
+						PluginMessage.toSender(sender, plugin, "New update is available: " + ChatColor.YELLOW + plugin.getDescription().getName() + " " + updater.getLatestVersion());
+
+						if(updater.getUpdate().getChanges() != null && updater.getUpdate().getChanges().isEmpty() == false)
+						{
+							PluginMessage.toSender(sender, plugin, "");
+							PluginMessage.toSender(sender, plugin, updater.getUpdate().getChanges(), ChatColor.GREEN);
+							PluginMessage.toSender(sender, plugin, "");
+						}
+
+						Downloader downloader = new Downloader(updater.getUpdate());
+						if(downloader.download())
+							PluginMessage.toSender(sender, plugin, "Update downloaded into plugins\\Updater ");
+
+						PluginMessage.toSender(sender, plugin, "");
+						PluginStraightMessage.toSender(sender, plugin, ChatColor.AQUA);
+						EmptyMessage.toSender(sender);
+					}
+					else if(response == Response.NO_UPDATE)
+					{
+						PluginMessage.toSender(sender, plugin, "You are running the latest version;", ChatColor.GREEN);
+					}
+					else if(response == Response.ERROR_TIME_OUT)
+					{
+						ErrorMessage.toSender(sender, "Time out will trying to find an update to " + plugin.getDescription().getName());
+					}
+					else
+					{
+						ErrorMessage.toSender(sender, "an error occured will trying to find an update to " + plugin.getDescription().getName());
+					}
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}, delay);
 	}
 }
