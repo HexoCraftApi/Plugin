e854461314a13c5dbf72799ef9e6c84ed7088b8e
hexosse <hexosse@gmail.com>
2016-09-22 15:47:08 +0200

    Optimized import Javadoc update Copyright update

--- src/main/java/com/github/hexocraftapi/plugin/Plugin.java
+++ src/main/java/com/github/hexocraftapi/plugin/Plugin.java
@@ -1,19 +1,19 @@
 package com.github.hexocraftapi.plugin;
 
 /*
- * Copyright 2015 hexosse
+ * Copyright 2016 hexosse
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import com.github.hexocraftapi.command.Command;
@@ -33,7 +33,7 @@ import org.bukkit.plugin.IllegalPluginAccessException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * @author <b>hexosse</b> (<a href="https://github.comp/hexosse">hexosse on GitHub</a>))
+ * @author <b>Hexosse</b> (<a href="https://github.com/hexosse">on GitHub</a>))
  */
 public abstract class Plugin extends JavaPlugin
 {
